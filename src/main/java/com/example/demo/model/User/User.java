ef5595665b9af0c21516e2f1ba5b755d264aa01e
504cam <106109561+504cam@users.noreply.github.com>
2022-07-07 10:39:44 -0500

    User controller, repo, getter/setter

--- src/main/java/com/example/demo/model/User/User.java
+++ src/main/java/com/example/demo/model/User/User.java
@@ -1,6 +1,7 @@
 package com.example.demo.model.User;
 
 import java.io.Serializable;
+import java.util.Objects;
 import javax.persistence.*;
 
 @Entity(name="users")
@@ -24,4 +25,71 @@ public class User implements Serializable {
 
         @Enumerated
         private Role role;
-    }
+
+        public User() {
+        }
+
+        public User(Integer userId, String firstName, String lastName, String username, String passWord, Role role) {
+                UserId = userId;
+                this.firstName = firstName;
+                this.lastName = lastName;
+                this.username = username;
+                this.passWord = passWord;
+                this.role = role;
+        }
+
+        public Integer getUserId() {
+                return UserId;
+        }
+
+        public User setUserId(Integer userId) {
+                UserId = userId;
+                return this;
+        }
+
+        public String getFirstName() {
+                return firstName;
+        }
+
+        public User setFirstName(String firstName) {
+                this.firstName = firstName;
+                return this;
+        }
+
+        public String getLastName() {
+                return lastName;
+        }
+
+        public User setLastName(String lastName) {
+                this.lastName = lastName;
+                return this;
+        }
+
+        public String getUsername() {
+                return username;
+        }
+
+        public User setUsername(String username) {
+                this.username = username;
+                return this;
+        }
+
+        public String getPassWord() {
+                return passWord;
+        }
+
+        public User setPassWord(String passWord) {
+                this.passWord = passWord;
+                return this;
+        }
+
+        public Role getRole() {
+                return role;
+        }
+
+        public User setRole(Role role) {
+                this.role = role;
+                return this;
+        }
+
+}
