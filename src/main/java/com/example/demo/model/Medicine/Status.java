55e840a28e5076ff34449e97472d79414c577b18
504cam <106109561+504cam@users.noreply.github.com>
2022-07-08 09:21:28 -0500

    delete

--- src/main/java/com/example/demo/model/Medicine/Status.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.model.Medicine;
-
-public enum Status {
-    In_Stock, Out_of_Stock, Low_Stock
-}
--- src/main/java/com/example/demo/model/Medicine/Type.java
+++
@@ -1,5 +0,0 @@
-package com.example.demo.model.Medicine;
-
-public enum Type {
-    Pill, Liquid, Drop, Inhaler
-}
