c32a8d73944c176c07328df0f4698b24a5161975
Andrew Beauchemin <android@vt.edu>
2022-07-20 02:41:08 -0400

    cross origin mapping

--- src/main/java/com/revature/controller/PaymentController.java
+++ src/main/java/com/revature/controller/PaymentController.java
@@ -18,27 +18,32 @@ public class PaymentController {
     public PaymentController(PaymentService paymentService){this.paymentService = paymentService;}
 
     @PostMapping
+    @CrossOrigin
     public Payment createPayment(@RequestBody Payment payment){
         return paymentService.createPayment(payment);
     }
 
     @GetMapping
+    @CrossOrigin
     public List<Payment> getAllPayments(){
         return paymentService.getAllPayment();
     }
 
     @GetMapping("/{id}")
+    @CrossOrigin
     public Payment getPaymentById(@PathVariable int id){
         return paymentService.getPaymentById(id);
     }
 
     @GetMapping("/paystatus/{Status}")
+    @CrossOrigin
     public List<Payment> getAllByPayStatus(@PathVariable("Status") String payStatus){
         PayStatus payStatus1 = PayStatus.valueOf(payStatus.toUpperCase(Locale.ROOT));
         return paymentService.getAllByPayStatus(payStatus1);
     }
 
     @GetMapping("/userid/{Id}")
+    @CrossOrigin
     public List<Payment> getAllByUserId(@PathVariable("Id") String id){
         return paymentService.getAllByUserId(Integer.parseInt(id));
     }
